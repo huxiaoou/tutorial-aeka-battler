9fbeb30a9b0672c5d82aeb44a0971fed3e49672d
huxiaoou <516984451@qq.com>
2026-04-28 23:18:40 +0800

    Add 3d animation

---
+++ resources/animation_unit.res
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:06b34f8f192dc7da5a97af06e85c4b2a6439a8fb43ca3dc8eecdbd065a24ae22
+size 552
--- unit.gd
+++ unit.gd
@@ -1,8 +1,3 @@
 extends CharacterBody2D
 
 class_name Unit
-
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
-func _ready() -> void:
-    animation_player.play("idle0")
